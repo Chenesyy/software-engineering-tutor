ed29ead0288a33594ff929a087a4952f5edaa719
Chenesyy <chesternevansy@gmail.com>
2022-11-09 03:43:08 +0800

    Added notes in arrays

---
+++ JavaScript/4-arrays.js
@@ -0,0 +1,11 @@
+//========== Arrays ==========//
+/*---*/
+let numbers_array = [1, 2, 3, 4];
+let alphabet_array = ['a', 'e', 'i', 'o', 'u'];
+let boolean_array = [true, true, false, true];
+/*---*/
+
+//map
+//find
+//forEach
+//filter
